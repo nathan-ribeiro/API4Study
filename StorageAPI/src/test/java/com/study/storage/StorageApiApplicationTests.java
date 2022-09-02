53de476807790995b148d733d53af1729b8c2b4a
Nathan <nathanribeiro778@gmail.com>
2022-09-02 14:44:33 -0300

    Iniciando o projeto

---
+++ StorageAPI/src/test/java/com/study/storage/StorageApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.storage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorageApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
